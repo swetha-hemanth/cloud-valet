bc7f23b6f42c9e148467f50c7e0df61267d1b668
swetha-hemanth <swethaswe0556@gmail.com>
2026-08-23 11:36:55 +0530

    Connect frontend to live Render backend

--- backend/main.py
+++ backend/main.py
@@ -149,7 +149,8 @@ app.add_middleware(
 
     allow_origins=[
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "https://swetha-hemanth.github.io"
     ],
 
     allow_credentials=True,
